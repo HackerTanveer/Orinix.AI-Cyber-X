41da7cea51e7037a8ca77627ddfb9e404ad8d99c
Tanveer <146033345+HackerTanveer@users.noreply.github.com>
2023-10-01 09:26:35 -0700

    update

---
+++ client/README.md
@@ -0,0 +1,1 @@
+Created by Cyber X Team.
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../App.css';
+import MainScreen from '../MainScreen';
+import Footer from '../Footer';
+
+function Home() {
+  return (
+    <>
+      <MainScreen />
+ 
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
